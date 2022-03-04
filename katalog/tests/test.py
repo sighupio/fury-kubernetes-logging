684a960f0c467661bb5c1eb24cb97170e86fa818
Samuele Chiocca <samuele@sighup.io>
2022-03-04 18:56:51 +0100

    Add apiextensions.k8s.io/v1 to tests.py

--- katalog/tests/test.py
+++ katalog/tests/test.py
@@ -37,6 +37,7 @@ def test_api_version(doc):
         "monitoring.coreos.com/v1",
         "rbac.authorization.k8s.io/v1",
         "apiextensions.k8s.io/v1beta1",
+        "apiextensions.k8s.io/v1",
         "logging.banzaicloud.io/v1beta1",
         "logging-extensions.banzaicloud.io/v1alpha1",
     ]
